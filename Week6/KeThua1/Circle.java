572876435274074ff3b7a5b36949f2bb4b585823
tHoangAnh1003 <t.hoang.1003.anh@gmail.com>
2024-10-11 08:23:16 +0700

    Week6/KeThua1

---
+++ Week6/KeThua1/Circle.java
@@ -0,0 +1,52 @@
+class Circle {
+    protected static final double PI = 3.14;
+    private double radius;
+    private String color;
+
+    public Circle() {
+        this.radius = 0;
+        this.color = "White";
+    }
+
+    /**
+     * Constructor radius.
+     */
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * Constructor radius and color.
+     */
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * Compute Area Value.
+     */
+    public double getArea() {
+        return radius * radius * PI;
+    }
+
+    public String toString() {
+        return "Circle[radius=" + radius + ",color=" + color + "]";
+    }
+} 
---
+++ Week6/KeThua1/Cylinder.java
@@ -0,0 +1,50 @@
+class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder() {
+        this.height = 0;
+    }
+
+    /**
+     * Constructor height.
+     */
+    public Cylinder(double height) {
+        this.height = height;
+    }
+
+    /**
+     * Constructor height and radius.
+     */
+    public Cylinder(double height, double radius) {
+        super(radius);
+        this.height = height;
+    }
+
+    /**
+     * Constructor height, radius and color.
+     */
+    public Cylinder(double height, double radius, String color) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * Compute Volume value.
+     */
+    public double getVolume() {
+        return getArea() * height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder[" + super.toString() + ",height=" + height + "]";
+    }
+} 
